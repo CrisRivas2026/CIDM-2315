0d247479716ca8d90c2d392d8dda13b1dcb878d2
CrisRivas2026 <cjrivas1@buffs.wtamu.edu>
2026-04-22 22:32:12 -0500

    Final Project

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Dynamic;
+
+namespace FinalProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("---CIDM2315 Final Project: Cristian Rivas---");
+        Console.WriteLine("---Welcome to Buff Hotel---");
+        
+        CheckAccount();
+
+
+
+    }
+
+    static void CheckAccount()
+    {
+        Console.WriteLine("-->Please input username: ");
+        string username = Console.ReadLine();
+        Console.WriteLine("-->Please input password: ");
+        string password = Console.ReadLine();
+
+        if (username == "cristian")
+        {
+            if (password == "cristian123")
+            {
+                Console.WriteLine("-->Login Successfully");
+            }
+            else
+            {
+                Console.WriteLine("Wrong Username/Password");
+            }
+            
+        }
+        
+        else
+        {
+            Console.WriteLine("Wrong Username/Password");
+        }
+    
+
+    }
+}
